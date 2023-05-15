b7e957e67a217a956357eddb212ff9c335bef7f3
sgrDs <sagardasdev@gmail.com>
2023-05-15 18:40:42 +0530

    changed the theme from dark to night

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" data-theme="dark">
+    <html lang="en" data-theme="night">
       <body className={inter.className + " " + "bg-base-200"}>
         <Navbar />
         {children}
