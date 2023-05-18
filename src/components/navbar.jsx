c6b541e42b8a421721fef38ecab88632ba24dfdb
sgrDs <sagardasdev@gmail.com>
2023-05-18 09:50:47 +0530

    Links are added to the navbar component

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,48 +1,35 @@
 import Link from "next/link";
 import React from "react";
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div className="mx-auto max-w-screen-2xl flex items-center p-2 min-h-[15vh] bg-base-200">
+    <div className="mx-auto max-w-screen-2xl flex justify-between items-center p-2 min-h-[15vh] bg-base-200">
       <div className="flex-1">
         <Link href="/" className="text-2xl md:text-3xl text-white font-medium">
           SagarCodes
         </Link>
       </div>
       <div className="flex-none">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
+        <ul className="menu-horizontal px-1 font-medium">
+          <li className="px-5 py-2">
+            <Link href="/blog">Blog</Link>
           </li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2 bg-base-100">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
+          <li className="px-5 py-2">
+            <Link href="/service">Service</Link>
           </li>
-          <li>
-            <a>Item 3</a>
+          <li className="px-5 py-2">
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li className="px-5 py-2">
+            <Link href="/about">About</Link>
           </li>
         </ul>
       </div>
+      {/* <div>
+        <h1>this is sagar</h1>
+      </div> */}
     </div>
   );
 };
 
-export default navbar;
+export default Navbar;
