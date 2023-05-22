a6ba3b0c782f872b06c58c1ed9283808dfd212be
sgrDs <sagardasdev@gmail.com>
2023-05-22 22:42:59 +0530

    created hero section for the  blogPage /blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const page = () => {
-  return <div>blog</div>;
+  return (
+    <div>
+      <div className="hero my-20 bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-4xl">
+            <h1 className="text-5xl font-bold">
+              Unlock the Secrets to Successful Marketing, Developing Product,
+              etc.🏆
+            </h1>
+            <p className="py-6 text-xl mt-4">
+              Your Guide to Building a Winning Strategy for marketing, or
+              building a Digital Product or to build a Perfect Story for your
+              business.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
