d922fab31ac9eea0170a3b3ea7949bd9b578851f
sgrDs <sagardasdev@gmail.com>
2023-05-25 03:29:25 +0530

    blogCard component is now dynamic

--- src/components/blogCard.jsx
+++ src/components/blogCard.jsx
@@ -1,37 +1,29 @@
 import Image from "next/image";
+import Link from "next/link";
 
-const BlogCard = (
-  {
-    //   leadingTeam,
-    //   article: {
-    //     readTime,
-    //     dateDisplay,
-    //     slug,
-    //     frontmatter,
-    //     frontmatter: {
-    //       title = "Untitled Post",
-    //       bannerCloudinaryId,
-    //       bannerBlurDataUrl,
-    //     },
-    //   },
-  }
-) => {
+const BlogCard = ({ post }) => {
+  // console.log(post);
+  const { slug, frontmatter } = post;
+  const { title, date, coverImage, time } = frontmatter;
+  console.log(frontmatter);
   return (
-    <div className="w-full group">
+    <Link href={"/blog/" + slug} className="w-full group">
       <div className="aspect-h-4 aspect-w-3 bg-red-200 rounded-lg overflow-hidden card group-hover:ring-2  ring-offset-base-200 ring-offset-4 group-hover:ring-warning">
         <Image
           width={1600}
           height={1000}
-          src="/wallpaper.jpg"
+          src={coverImage}
           className="object-cover h-full"
           alt="HeroImage"
         />
       </div>
       <div>
-        <p className="my-6 text-xl font-medium">May 8th, 2023 — 5 min read</p>
-        <h3 className="text-3xl text-white font-medium">When to useMemo</h3>
+        <p className="my-6 text-xl font-medium">
+          {date} — {time} min read
+        </p>
+        <h3 className="text-3xl text-white font-medium">{title}</h3>
       </div>
-    </div>
+    </Link>
   );
 };
 
