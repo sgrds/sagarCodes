6084bf0111d8f44437aa14c8672d989a1c7a7972
sgrDs <sagardasdev@gmail.com>
2023-05-27 03:52:49 +0530

    random blog selection function created

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,11 @@
 export const sortByDate = (a, b) => {
   return new Date(b.frontmatter.date) - new Date(a.frontmatter.date);
 };
+export const randomBlogReturn = (arr, max = 4) => {
+  const randomBlogs = new Set();
+  for (let i = 0; i < max; i++) {
+    const randomBlog = arr[Math.floor(Math.random() * (arr.length + 1))];
+    randomBlogs.add(randomBlog);
+  }
+  return randomBlogs;
+};
