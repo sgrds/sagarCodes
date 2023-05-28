a6a71996b0b888659e055b4664039fe3c3a66bae
sgrDs <sagardasdev@gmail.com>
2023-05-28 13:07:41 +0530

    const to store featured blogs

---
+++ src/constants/index.js
@@ -0,0 +1,46 @@
+export const featuredBlogs = [
+  {
+    slug: "new-in-php-8",
+    frontmatter: {
+      title: "What's New In PHP 8?",
+      date: "March 7, 2021",
+      excerpt:
+        "In this article we will look at some of the new features offered in version 8 of PHP",
+      coverImage: "/wallpaper.jpg",
+      time: 10,
+    },
+  },
+  {
+    slug: "python-book-review",
+    frontmatter: {
+      title: "Python Book Review",
+      date: "March 7, 2021",
+      excerpt:
+        "In this review, we will be looking at the new Python book from Oriley",
+      coverImage: "/wallpaper.jpg",
+      time: 10,
+    },
+  },
+  {
+    slug: "django-crash-course",
+    frontmatter: {
+      title: "Django Crash Course",
+      date: "March 5, 2021",
+      description:
+        "Django is a very powerful, high level Python framework for building web applications",
+      coverImage: "/wallpaper.jpg",
+      time: 10,
+    },
+  },
+  {
+    slug: "javascript-performance-tips",
+    frontmatter: {
+      title: "JavaScript Performance Tips",
+      date: "March 4, 2021",
+      excerpt:
+        "We will look at 10 simple tips and tricks to increase the speed of your code when writing JS",
+      coverImage: "/wallpaper.jpg",
+      time: 10,
+    },
+  },
+];
