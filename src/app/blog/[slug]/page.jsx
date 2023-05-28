e8fae27954240fb6440be1084634aa67214c86a8
sgrDs <sagardasdev@gmail.com>
2023-05-28 13:19:30 +0530

    added blogSection & link to github and twitter at the end of the blog (/blog/[slug])

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -4,6 +4,8 @@ import { Spacer } from "@/components/spacer";
 import BlogHeader from "@/components/sections/blogHeader";
 import { bundleMDX } from "mdx-bundler";
 import { getMDXComponent } from "mdx-bundler/client";
+import BlogSection from "@/components/sections/blogSection";
+import Link from "next/link";
 export const dynamicParams = false;
 
 async function getBlogs(slug) {
@@ -33,7 +35,6 @@ async function getBlogs(slug) {
 export default async function Blogs({ params: { slug } }) {
   const { code, frontmatter } = await getBlogs(slug);
   const Component = getMDXComponent(code);
-  // console.log(frontmatter);
 
   return (
     <main className="px-4 mx-auto max-w-screen-xl pt-8 pb-16 lg:pt-16 lg:pb-24 ">
@@ -42,7 +43,26 @@ export default async function Blogs({ params: { slug } }) {
         <div className="prose md:prose-lg dark:prose-invert mx-auto w-full max-w-3xl">
           <Component />
         </div>
-        <Spacer size="2xs" />
+        <Spacer size="sm" />
+        <div className="mx-auto max-w-3xl border-b-2 pb-4 border-neutral justify-end flex gap-4">
+          <a
+            target="_blank"
+            href={`https://twitter.com/intent/tweet?url=https://sagarcodes.com/blog/${slug}`}
+            className="text-error hover:underline font-medium"
+          >
+            Tweet this article
+          </a>
+          <a
+            target="_blank"
+            href={`https://github.com/sgrds/sagarCodes/blob/main/posts/${slug}.mdx`}
+            className="text-error hover:underline font-medium"
+          >
+            Edit on Github
+          </a>
+        </div>
+
+        <Spacer size="sm" />
+        <BlogSection />
       </article>
     </main>
   );
