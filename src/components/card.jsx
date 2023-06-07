0ff29f7069f409439754f9e768f5e85b37ecba2a
sgrDs <sagardasdev@gmail.com>
2023-06-08 00:02:03 +0530

    text, ring & button colour changed in cardComponent

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -8,7 +8,7 @@ const Card = ({
   link = "/404",
 }) => {
   return (
-    <div className="aspect-h-4 aspect-w-3 hover:ring-2  ring-offset-base-100 ring-offset-4 hover:ring-primary hover:duration-300 hover:ease-in rounded-lg shadow overflow-hidden">
+    <div className="group aspect-h-4 aspect-w-3 hover:ring-2  ring-offset-base-100 ring-offset-4 hover:ring-primary hover:duration-300 hover:ease-in rounded-lg shadow overflow-hidden bg-secondary-content">
       <div className="grid">
         <div className="aspect-h-9 aspect-w-16 overflow-hidden">
           <Image
@@ -20,13 +20,13 @@ const Card = ({
           />
         </div>
         <div className="p-5 text-center grid content-end">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight line-clamp-2">
+          <h5 className="group-hover:text-success mb-2 text-2xl font-bold tracking-tight line-clamp-2">
             {title}
           </h5>
           <Link
             href={link}
             target="_blank"
-            className="mt-4 btn btn-outline btn-warning btn-lg rounded-full"
+            className="mt-4 btn btn-outline btn-success btn-lg rounded-full"
           >
             {button}
           </Link>
