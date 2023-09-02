886d9b80acff3b3b3042829c66ffe83a0836aa01
sgrDs <sagardasdev@gmail.com>
2023-09-02 21:35:55 +0530

    added metadata, changed book a call button & add BookCall section in the client page

--- src/app/clients/page.jsx
+++ src/app/clients/page.jsx
@@ -1,7 +1,14 @@
 import BlogCard from "@/components/blogCard";
 import { Spacer } from "@/components/spacer";
 import { getAllFileNameAndFrontmatter } from "@/utils/file";
+import { BookCall } from "../contact/page";
+import Link from "next/link";
 
+export const metadata = {
+  title: "Clients - SagarCodes",
+  description:
+    "We've had the opportunity to help numerous clients. We're a small team that works hard to create successful digital projects, develop creative digital products, and help your business grow.",
+};
 const page = async () => {
   const { posts } = await getAllFileNameAndFrontmatter("clientList", "mdx");
 
@@ -16,7 +23,17 @@ const page = async () => {
             </h1>
             <Spacer size="xs" />
 
-            <p className="py-4 text-xl">My clients from different industries</p>
+            {/* <p className="text-xl">My clients from different industries</p> */}
+            <div className="grid justify-center">
+              <Link
+                target="_blank"
+                href="https://calendly.com/sagarcodes/30min"
+              >
+                <button className="block mt-11 btn btn-active btn-primary btn-lg rounded-full">
+                  Book a Call
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
@@ -26,12 +43,19 @@ const page = async () => {
       {/* blog list */}
 
       <div className="mx-4">
-        <div className="mx-auto max-w-screen-xl grid gap-6 md:grid-cols-2 lg:grid-cols-3 grid-flow-row-dense">
+        <div className="mx-6 sm:mx-auto max-w-screen-xl grid gap-6 md:grid-cols-2 lg:grid-cols-3 grid-flow-row-dense">
           {posts.map((data, index) => (
             <BlogCard key={index} link="clients" post={data} />
           ))}
         </div>
+        <Spacer size="xs" />
+        <div className="text-center font-medium text-gray-500">
+          {`You've reached the End of the List 😓`}
+        </div>
         {/* Spacer component */}
+        <Spacer size="base" />
+
+        <BookCall />
         <Spacer size="lg" />
       </div>
     </div>
