6ccd7dd9338d3239b966fc41a60142ca120a50ad
sgrDs <sagardasdev@gmail.com>
2023-09-02 22:12:39 +0530

    discord section completed

--- src/components/sections/discordSection.jsx
+++ src/components/sections/discordSection.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { SiDiscord } from "react-icons/si";
 
 const DiscordSection = () => {
@@ -11,19 +12,24 @@ const DiscordSection = () => {
             Discord
           </div>
           <p className="text-5xl text-white mt-16">
-            Meet like minded people facing similar challenges.
+            Ready to chat and connect on Discord - Come say hello!
           </p>
-          <p className="text-5xl my-8">Join our server now.</p>
-          <button className="mt-4 btn btn-outline btn-warning btn-lg rounded-full">
-            Join Discord
-          </button>
+          {/* <p className="text-5xl my-8">Join our server now.</p> */}
+          <Link
+            href="https://discordapp.com/users/1113083414518694019"
+            target="_blank"
+          >
+            <button className="mt-16 btn btn-outline btn-warning btn-lg rounded-full">
+              CHAT ON DISCORD
+            </button>
+          </Link>
         </div>
-        <div className="aspect-h-6 aspect-w-5 rounded-lg overflow-hidden">
+        <div className="grid items-center justify overflow-hidden">
           <Image
-            width={500}
-            height={1500}
-            src="/noticeBoard.jpg"
-            className="object-cover"
+            width={600}
+            height={600}
+            src="/discord.png"
+            // className="object-cover"
             alt="HeroImage"
           />
         </div>
